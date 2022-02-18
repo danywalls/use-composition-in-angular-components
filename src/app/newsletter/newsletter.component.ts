c4bbee5fad7cf63f3339bc98aff69d5d33ae5fde
danywalls <danywalls4@gmail.com>
2022-02-18 12:17:44 +0100

    move-to-herency

--- src/app/newsletter/newsletter.component.ts
+++ src/app/newsletter/newsletter.component.ts
@@ -1,20 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBase } from '../bussines/form-base';
 
 @Component({
   selector: 'app-newsletter',
   templateUrl: './newsletter.component.html',
   styleUrls: ['./newsletter.component.css'],
 })
-export class NewsletterComponent implements OnInit {
-  constructor() {}
+export class NewsletterComponent extends FormBase implements OnInit {
+  constructor() {
+    super();
+  }
 
   ngOnInit() {}
 
   save() {
     console.log('saving');
     this.sendToAnalytics();
-  }
-  sendToAnalytics() {
-    console.log('Sending to analytics');
   }
 }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,20 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBase } from '../bussines/form-base';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css'],
 })
-export class SignupComponent implements OnInit {
-  constructor() {}
+export class SignupComponent extends FormBase implements OnInit {
+  constructor() {
+    super();
+  }
 
   ngOnInit() {}
 
   save() {
     console.log('saving');
     this.sendToAnalytics();
-  }
-  sendToAnalytics() {
-    console.log('Sending to analytics');
   }
 }
