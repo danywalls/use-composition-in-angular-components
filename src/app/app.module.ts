cd13ffc58e1c707cab4d73889da4b39c085b309a
danywalls <danywalls4@gmail.com>
2022-02-19 08:49:38 +0100

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { SignupComponent } from './signup/signup.component';
-import { NewsletterComponent } from './newsletter/newsletter.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [
-    AppComponent,
-    HelloComponent,
-    SignupComponent,
-    NewsletterComponent,
-  ],
+  declarations: [AppComponent, HelloComponent, SignupComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
